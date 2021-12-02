2e765b54c1e1acd1d68b5bbdbdd60a9ebd0a2dc2
VladNewt <59644601+VladNewt@users.noreply.github.com>
2021-12-02 20:22:22 +0500

    Final commit 20:21 02.12.21

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,7 +1,30 @@
 package pack;
 
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Path path = Paths.get("file.txt");
+
+        String text = "Lorem ipsum dolor sit amet.";
+
+        //Запись в файл
+        try {
+            Files.write(path,text.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Чтение из файла
+        try {
+            System.out.println(Files.readString(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
